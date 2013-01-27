410d0a998b64985afedebd50bf49f190155700a2
Michael Strüder <mikezter@gmail.com>
2013-01-27 21:17:55 +0100

    change stuff

--- balanced_smileys.py
+++ balanced_smileys.py
@@ -16,6 +16,10 @@ def balanced(line):
     pre = line[:m.start()]
     inner = line[m.start() + 1:m.end() - 1]
     post = line[m.end():]
+    print pre
+    print inner
+    print post
+
 
     return balanced(pre) & balanced(pre) & balanced(post)
 
