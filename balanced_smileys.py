c08f0feccef0981d81f8833b86c939b28bd78faa
Michael Strüder <mikezter@gmail.com>
2013-01-27 21:06:53 +0100

    change stuff

--- balanced_smileys.py
+++ balanced_smileys.py
@@ -10,7 +10,7 @@ def balanced(line):
   if line == "": return True
   if re.match(r"^[a-z :]+$", line): return True
 
-  m = re.search(r"\(.*\)")
+  m = re.search(r"\(.*\)", line)
 
   if m:
     pre = line[:m.start()]
