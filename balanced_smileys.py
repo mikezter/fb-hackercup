a4bd28dcfec8786beec11c1919e00482b1d9128c
Michael Strüder <mikezter@gmail.com>
2013-01-27 21:06:24 +0100

    change stuff

--- balanced_smileys.py
+++ balanced_smileys.py
@@ -10,6 +10,18 @@ def balanced(line):
   if line == "": return True
   if re.match(r"^[a-z :]+$", line): return True
 
+  m = re.search(r"\(.*\)")
+
+  if m:
+    pre = line[:m.start()]
+    inner = line[m.start() + 1:m.end() - 1]
+    post = line[m.end():]
+
+    return balanced(pre) & balanced(pre) & balanced(post)
+
+  return False
+
+
 
 def answer(answer):
   if answer: return "YES"
