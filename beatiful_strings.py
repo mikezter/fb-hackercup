ee3455bcafa553731056797af7a689436b56671f
Michael Strüder <mikezter@gmail.com>
2013-01-27 18:53:10 +0100

    change stuff

--- beatiful_strings.py
+++ beatiful_strings.py
@@ -2,16 +2,16 @@ import sys
 
 line = sys.stdin.readline()
 
-def score(line):
+def counts(line):
   counts = dict.fromkeys(list(line), 0)
   for c in line:
     counts[c] = counts[c] + 1
 
-  return counts["A"]
+  return counts
 
 line = sys.stdin.readline().rstrip()
 while line:
   print list(line)
-  print score(line)
+  print counts(line)
   line = sys.stdin.readline().rstrip()
 
