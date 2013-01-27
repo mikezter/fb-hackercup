206b8461af07c814d8e42996a163c4daa3afada5
Michael Strüder <mikezter@gmail.com>
2013-01-27 20:05:58 +0100

    balanced smileys

---
+++ balanced_smileys.py
@@ -0,0 +1,20 @@
+import sys
+import re
+
+cases = int(sys.stdin.readline())
+
+def readline():
+  return sys.stdin.readline().rstrip()
+
+def balanced(line):
+  if line == "": return True
+  if re.match(r"^[a-z :]$", line): return True
+
+def answer(answer):
+  if answer: return "YES"
+  return "NO"
+
+for i in range(cases):
+  line = readline()
+  print "Case #" + str(i + 1) + ": " + answer(balanced(line))
+
