bab39c7d41138e14701893df89847fbcfc7b2ec9
Michael Strüder <mikezter@gmail.com>
2013-01-27 21:13:13 +0100

    change stuff

--- balanced_smileys.py
+++ balanced_smileys.py
@@ -8,7 +8,7 @@ def readline():
 
 def balanced(line):
   if line == "": return True
-  if re.match(r"^[a-z :]+$", line): return True
+  if re.match(r"^([a-z :]|:\)|:\()+$", line): return True
 
   m = re.search(r"\(.*\)", line)
 
@@ -20,8 +20,6 @@ def balanced(line):
     return balanced(pre) & balanced(pre) & balanced(post)
 
   return False
-
-
 
 def answer(answer):
   if answer: return "YES"
