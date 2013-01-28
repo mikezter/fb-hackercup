2421dda0ea284a130290507a4c5fc08224e4389f
Michael Strüder <mikezter@gmail.com>
2013-01-28 21:42:43 +0100

    change stuff

--- find_min.py
+++ find_min.py
@@ -5,14 +5,16 @@ cases = int(sys.stdin.readline())
 def readline():
   return sys.stdin.readline().rstrip()
 
-def buildlist(a, b, c, d, length, knowns):
-  l = list(length)
-  for i in range(knowns):
-    if i == 0: l[i] = a
+def buildlist(a, b, c, d, knowns):
+  m = list(knowns)
+  m[0] = a
+  for i in range(knowns - 1):
+    m[i + 1] = (b * m[i] + c) % r
 
-
+  return m
 
 for i in range(cases):
   length, knowns = readline().split()
   a, b, c, d = readline().split()
+  print buildlist(a, b, c, d, knowns)
   print "Case #" + str(i + 1) + ": " + answer(balanced(line))
