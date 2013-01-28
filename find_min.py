aeb25d6bf3d534ba8f65097198575a7bfe724120
Michael Strüder <mikezter@gmail.com>
2013-01-28 21:51:48 +0100

    change stuff

--- find_min.py
+++ find_min.py
@@ -8,7 +8,7 @@ def readline():
 def buildlist(a, b, c, r, k):
   m = []
   m.append(a)
-  for i in range(knowns - 1):
+  for i in range(k - 1):
     m.append((b * m[i] + c) % r)
 
   return m
@@ -17,5 +17,5 @@ for i in range(cases):
   n, k = [int(i) for i in readline().split()]
   a, b, c, r = [int(i) for i in readline().split()]
 
-  print buildlist(a, b, c, r, knowns)
+  print buildlist(a, b, c, r, k)
   print "Case #" + str(i + 1) + ": "
