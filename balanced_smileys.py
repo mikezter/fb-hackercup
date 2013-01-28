8087054f61a174b8f980df62cf738e3b9a5c1fb9
Michael Strüder <mikezter@gmail.com>
2013-01-28 02:21:09 +0100

    change stuff

--- balanced_smileys.py
+++ balanced_smileys.py
@@ -6,8 +6,7 @@ def readline():
   return sys.stdin.readline().rstrip()
 
 def invalid(c):
-  print ord(c)
-  return ord(c) not in range(97, 110) or c not in [' ', ':', '(', ')']
+  return ord(c) not in range(97, 110) and c not in [' ', ':', '(', ')']
 
 def balanced(line):
   was_colon = False
